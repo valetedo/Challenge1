d5fadd85481a00e5aeb88d91e15745bddf8828fa
valetedo <valetedo@users.noreply.github.com>
2020-06-24 16:33:38 +0200

    Add files via upload

---
+++ deploy.ps1
@@ -0,0 +1,11 @@
+
+
+param([Parameter(Mandatory)][string] $GitHubRepo = "https://github.com/microsoft/TailwindTraders-Website", 
+
+        [string] $branch = "main", 
+
+        [Parameter(Mandatory)][string] $AppServiceName, 
+
+        [Parameter(Mandatory)][string] $ResourceGroupName)
+
+az webapp deployment source config --branch $branch --manual-integration --name $AppServiceName --repo-url $GitHubRepo --resource-group $ResourceGroupName
